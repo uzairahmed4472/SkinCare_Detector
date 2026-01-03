463238dfd0349f4b2f4747e00dce93ff98f3f255
uzairahmed4472 <ahmeduzairmarwat@gmail.com>
2026-01-03 23:27:45 +0500

    login and register updated

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -3,30 +3,59 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_application_1/modules/login_screen/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:splashscreen/splashscreen.dart';
 
-class Splash2 extends StatelessWidget {
+class Splash2 extends StatefulWidget {
+  @override
+  _Splash2State createState() => _Splash2State();
+}
+
+class _Splash2State extends State<Splash2> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SecondScreen()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SplashScreen(
-        gradientBackground: LinearGradient(
-          colors: [
-            Color.fromARGB(146, 147, 226, 255),
-            Color.fromARGB(255, 227, 245, 255)
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(146, 147, 226, 255),
+              Color.fromARGB(255, 227, 245, 255)
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
         ),
-        seconds: 4,
-        navigateAfterSeconds: const SecondScreen(),
-        image: Image.asset('assets/logo2.png', fit: BoxFit.fill),
-        photoSize: 200.0,
-        loadingText: Text(
-          "We care about your health",
-          style: TextStyle(color: Colors.blueAccent),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo2.png',
+                width: 200.0,
+                height: 200.0,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "We care about your health",
+                style: TextStyle(color: Colors.blueAccent),
+              ),
+              SizedBox(height: 20),
+              CircularProgressIndicator(
+                color: Color.fromARGB(255, 227, 245, 255),
+              ),
+            ],
+          ),
         ),
-        loaderColor: Color.fromARGB(255, 227, 245, 255),
       ),
     );
   }
